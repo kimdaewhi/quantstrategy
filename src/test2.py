6fb9af1700675b3468747fdde662030cd9414658
kimdaewhi <kimdaewhi@naver.com>
2024-06-10 16:29:45 +0900

    commit

--- src/test2.py
+++ src/test2.py
@@ -3,6 +3,7 @@ import os
 import sys
 import pandas as pd
 import FinanceDataReader as fdr
+from tabulate import tabulate
 
 
 # main 함수
@@ -10,5 +11,9 @@ if __name__ == '__main__':
     # DataFrame
     kospi = fdr.StockListing('KOSPI')
     kosdaq = fdr.StockListing('KOSDAQ')
+    
+    # 관리종목
+    adm_stock = fdr.StockListing('KRX-ADMIN')
 
-    kospi
+    # print(tabulate(kospi, headers='keys', tablefmt='fancy_outline'))
+    print(tabulate(adm_stock, headers='keys', tablefmt='fancy_outline'))
